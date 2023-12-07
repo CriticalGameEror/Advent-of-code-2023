d51cec74726a30a75f6a6adbe647c080b6eb46ce
CriticalGameEror <83310221+CriticalGameEror@users.noreply.github.com>
2023-12-07 18:04:23 +0000

    Day 7 completed

---
+++ Day 7/day_7_part_1.py
@@ -0,0 +1,59 @@
+from collections import Counter
+
+with open("input.txt", "r") as f:
+    file_input = f.readlines()
+
+hands = {}
+for line in file_input:
+    line = line.strip().split(" ")
+    hands[line[0]] = int(line[1])
+
+# block below sorts each hand into each of the categories
+hand_types = {"five_kind":[], "four_kind":[], "full":[], "three_kind":[], "two_pair":[], "one_pair":[], "high":[]}
+for hand in hands.keys():
+    count = Counter(hand)
+    if len(count) == 1:
+        hand_types["five_kind"].append(hand)
+    elif len(count) == 2:
+        if 4 in count.values():
+            hand_types["four_kind"].append(hand)
+        else:
+            hand_types["full"].append(hand)
+    elif len(count) == 3:
+        if 3 in count.values():
+            hand_types["three_kind"].append(hand)
+        else:
+            hand_types["two_pair"].append(hand)
+    elif len(count) == 4:
+        hand_types["one_pair"].append(hand)
+    else:
+        hand_types["high"].append(hand)
+
+# block below is for sorting the hands so higher weighted hands are given a higher rank in the same category
+card_types = "AKQJT987654321"
+sorting_order = dict(zip(card_types, range(50,35,-1)))
+def convert_to_number(hand):
+    temp = ""
+    for letter in hand:
+        temp += str(sorting_order[letter])
+    return int(temp)
+
+total = 0
+position = 1
+for key in reversed(hand_types.keys()):
+    if len(hand_types[key]) == 0:
+        continue
+    elif len(hand_types[key]) == 1:
+        total += position * hands[hand_types[key][0]]
+        position += 1
+    else:
+        order = sorted(hand_types[key], key=convert_to_number)
+        for hand in order:
+            total += position * hands[hand]
+            position += 1
+
+print(total)
+
+
+    
+
